44a391e198891d5c003498762e10161a54986a0a
Joemar <jbagalanon@gmail.com>
2020-10-05 01:28:28 +0800

    return the value of  car query

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,22 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-
+            var cars = ProcessFile("Fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
         }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return 
+            File.ReadAllLines(path)
+                .Skip(1) //skip 1 string array to avoid processing. Partition operator
+                .Where(line => line.Length > 1) //if more than 1 this is valid
+                .Select(Car.ParseFromCsv) //invoke method from model
+                .ToList();
+        }
+
     }
 }
