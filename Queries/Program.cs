b699a72e16cefef77d687e9bbc114d5b639324ee
Joemar <jbagalanon@gmail.com>
2020-10-04 14:39:47 +0800

    This custome filter is not working

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Queries.MyLinq;
 
 namespace Queries
 {
@@ -10,12 +11,29 @@ namespace Queries
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // initialte movie data
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var movies = new List<Movie>
+            {
+                new Movie {Title = "The Dark Knight", Rating = 8.9F, Year = 2008},
+                new Movie {Title = "The King's Speech", Rating = 8.9F, Year = 2018},
+                new Movie {Title = "Darkest Hours", Rating = 8.9F, Year = 2005},
+                new Movie {Title = "Cassanova", Rating = 7.9F, Year = 1958},
+                new Movie {Title = "Return Of the Living Dead", Rating = 9.2F, Year = 2017},
+                new Movie {Title = "Star Wars Return", Rating = 8.1F, Year = 1989}
+            };
+
+
+
+
+            //extension method
+
+            var query = movies.Filter(m => m.Year > 2000);
+
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
         }
     }
 }
