f02d5b89bd020285408cce2c2298d41a9b5a7382
Joemar <jbagalanon@gmail.com>
2020-10-04 12:46:26 +0800

    using variable and  modifyin each query

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -40,41 +40,34 @@ namespace Employees
             Console.WriteLine();
             #endregion
 
-
-            IEnumerable<Employee> developers = new Employee[]
-            {
+            var developers = new Employee[]
+                        {
                 new Employee {Id = 1, Name = "Joms"},
                 new Employee {Id = 2, Name = "Sem"},
                 new Employee {Id = 3, Name = "Richie"},
                 new Employee {Id = 4, Name = "Roouch"}
-            };
+                        };
 
-            IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee {Id = 5, Name = "Shrek"},
                 new Employee {Id = 6, Name = "Rick"}
             };
 
-            Console.WriteLine(developers.Count());
-            IEnumerator<Employee> enumerator = developers.GetEnumerator();
-
-            while (enumerator.MoveNext())
-            {
-                Console.WriteLine(enumerator.Current.Name);
-            }
-
 
             Console.WriteLine("Beginning of lambda arrange by name");
-            foreach (var employee in developers.Where(e=>e.Name.Length==6).OrderBy(e=>e.Name))
+            var query = developers.Where(e => e.Name.Length == 6)
+                .OrderBy(e => e.Name);
+
+            foreach (var employee in query)
 
             {
-             
+
                 Console.WriteLine(employee.Name);
 
 
-
             }
-       
+
         }
 
         private static bool NameStartsWithS(Employee employee)
