9fb4391eb02099bd7f88f4b17be94edc0fa5a957
Joemar <jbagalanon@gmail.com>
2020-10-05 01:45:00 +0800

    Recommended Query Implementation

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -20,12 +20,24 @@ namespace Cars
 
         private static List<Car> ProcessFile(string path)
         {
+            /*
+            //first solution
             return 
             File.ReadAllLines(path)
                 .Skip(1) //skip 1 string array to avoid processing. Partition operator
                 .Where(line => line.Length > 1) //if more than 1 this is valid
                 .Select(Car.ParseFromCsv) //invoke method from model
                 .ToList();
+                */
+
+            //recommended implementation
+
+            var query = from line in File.ReadAllLines(path)
+                    .Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+
+            return query.ToList();
         }
 
     }
