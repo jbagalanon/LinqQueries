2a120f688ca94f98949424bd5a9eb0de9ba8d492
Joemar <jbagalanon@gmail.com>
2020-10-04 11:20:29 +0800

    functio single and dual input

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Employees;
 
 namespace Employees
 {
@@ -12,17 +11,31 @@ namespace Employees
         static void Main(string[] args)
         {
            
+            //takes integers and output integers
+            Func<int, int> square= x => x * x;
+            Console.WriteLine(square(3));
+            Console.WriteLine("End of multiplication function");
+            Console.WriteLine();
+
+
+            //takes two integers and output integers
+            Func<int, int, int> add = (int x, int y) => x + y;
+            Console.WriteLine(square(add(3,5)));
+            Console.WriteLine("End of dual input");
+            Console.WriteLine();
 
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Joms"},
                 new Employee {Id = 2, Name = "Sem"},
-                new Employee {Id = 3, Name = "Richie"}
+                new Employee {Id = 3, Name = "Richie"},
+                new Employee {Id = 4, Name = "Roouch"}
             };
 
             IEnumerable<Employee> sales = new List<Employee>()
             {
-                new Employee {Id = 4, Name = "Shrek"}
+                new Employee {Id = 5, Name = "Shrek"},
+                new Employee {Id = 6, Name = "Rick"}
             };
 
             Console.WriteLine(developers.Count());
@@ -37,8 +50,9 @@ namespace Employees
                 => e.Name.StartsWith("S")  
             ))
             {
-                Console.WriteLine(employee.Name);
                 Console.WriteLine("");
+                Console.WriteLine(employee.Name);
+              
 
             }
  
