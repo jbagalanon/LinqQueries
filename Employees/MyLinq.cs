e993df8f8c504966862d2f6704a5a9e0dfcb8f5c
Joemar <jbagalanon@gmail.com>
2020-10-04 10:41:25 +0800

    Count and List Employee

---
+++ Employees/MyLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public static  class MyLinq
+    {
+        public static int Count<T>(IEnumerable<T>sequence)
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+
+            return count;
+
+        }
+    }
+}
--- Employees/Program.cs
+++ Employees/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Employees;
 
 namespace Employees
 {
@@ -15,27 +16,38 @@ namespace Employees
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Joms"},
-                new Employee {Id = 2, Name = "Jem"},
+                new Employee {Id = 2, Name = "Sem"},
                 new Employee {Id = 3, Name = "Richie"}
             };
 
             IEnumerable<Employee> sales = new List<Employee>()
             {
-                new Employee {Id = 4, Name = "Alex"}
+                new Employee {Id = 4, Name = "Shrek"}
             };
 
-
-            foreach (var person in developers)
-            {
-                Console.WriteLine(person.Name);
-            }
-
+            Console.WriteLine(developers.Count());
             IEnumerator<Employee> enumerator = developers.GetEnumerator();
 
             while (enumerator.MoveNext())
             {
                 Console.WriteLine(enumerator.Current.Name);
             }
+
+            foreach (var employee in developers.Where(e
+                => e.Name.StartsWith("S")  
+            ))
+            {
+                Console.WriteLine(employee.Name);
+                Console.WriteLine("");
+
+            }
+ 
+      
+        }
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S"); 
         }
     }
 }
