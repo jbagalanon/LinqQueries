1312d271e5e0350b5c025ee1fb24bf8ad54376da
Joemar <jbagalanon@gmail.com>
2020-10-06 11:30:34 +0800

    BasicAggregateResult Done

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -15,17 +15,21 @@ namespace Cars
             var cars = ProcessCars("Fuel.csv");
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
-            var query = from manufacturer in manufacturers
-                join car in cars on manufacturer.Name equals car.Manufacturer
-                    into carGroup
-                orderby manufacturer.Name
+            var query = from car in cars
+                group car by car.Manufacturer
+                into carGroup
                 select new
                 {
-                    Manufacturer = manufacturer,
-                    Cars = carGroup
+                    Name = carGroup.Key,
+                    Max = carGroup.Max(c => c.Combined),
+                    Min = carGroup.Min(c => c.Combined),
+                    Avg = carGroup.Average(c => c.Combined)
+
                 }
                 into result
-                group result by result.Manufacturer.Headquarters;
+                orderby result.Max descending
+                select result;
+
 
             var query2 = manufacturers.GroupJoin(cars, m => m.Name,
                     c => c.Manufacturer, (m, g) =>
@@ -37,16 +41,14 @@ namespace Cars
                 .GroupBy(m => m.Manufacturer.Headquarters);
 
 
-            foreach (var group in query2)
+            foreach (var result in query)
             {
-                Console.WriteLine($"{group.Key} ");
+                Console.WriteLine($"{result.Name} ");
+                Console.WriteLine($"\t Max: {result.Max} ");
+                Console.WriteLine($"\t Min: {result.Min} ");
+                Console.WriteLine($"\t Average: {result.Avg} ");
 
-                foreach (var car in group.SelectMany(c => c.Cars)
-                    .OrderByDescending(c=>c.Combined).Take(3))
-
-                {
-                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
-                }
+              
             }
 
 
